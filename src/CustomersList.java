6323e8e412862341cd6c5537e49d6e09f367a914
galit <galitlad@walla.co.il>
2020-08-29 14:52:04 +0300

    Initial commit

---
+++ src/CustomersList.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class CustomersList {
+
+    public static void main(String[] args) {
+
+        ArrayList customer = new ArrayList();
+
+        customer.add("avi");
+        customer.add("zvi");
+        customer.add("david");
+        customer.add("yosi");
+        customer.add("sara");
+
+        System.out.println(customer);
+    }
+
+}
